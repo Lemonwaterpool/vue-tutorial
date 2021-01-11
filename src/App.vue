43eabaad30b19a39b6f1cca46909822e24c9e490
Lemonwaterpool <frqk3624@gmail.com>
2021-01-11 21:21:06 +0900

    chapter 4

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <user-profile/>
+</template>
+
+<script>
+import UserProfile from "./components/UserProfile";
+
+export default {
+  name: "App",
+  components: { UserProfile }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+}
+</style>
---
+++ src/components/TweetItem.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="tweet-item" @click="favouriteTweet(tweet.id)">
+    <div class="tuser-profile__tweet">
+      <div class="tweet-item__user">@{{ username }}</div>
+      <div class="tweet-item__content">
+        {{ tweet.content }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TweetItem",
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    tweet: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    favouriteTweet(id) {
+      this.$emit('favourite', id)
+    }
+  }
+};
+</script>
+
+<style>
+.tweet-item {
+  padding: 20px;
+  margin: 10px 5px;
+  background-color: white;
+  border-radius: 5px;
+  border-right: 5px;
+  border: 1px solid #dfe3e8;
+  box-sizing: border-box;
+  cursor: pointer;
+  transition: all 0.25s ease;
+  text-align: start;
+}
+
+.tweet-item:hover {
+  transform: scale(1.1, 1.1);
+}
+
+.tweet-item__user {
+  font-weight: bold;
+}
+</style>
